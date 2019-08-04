5c54feea11fbaba56002d06c612fa721d0e95d2c
Breeze <805511939@qq.com>
2019-08-04 11:18:25 +0800

    chore(skeletons): prettier codes
    refer #24

--- src/skeletons/ArticleListSkeleton.tsx
+++ src/skeletons/ArticleListSkeleton.tsx
@@ -5,30 +5,30 @@ import SpanSkeleton from './SpanSkeleton'
 import '../styles/Skeletons.less'
 
 interface IArticleListSkeletonProps {
-    className?: string
-    row: number
-    style?: CSSProperties
+  className?: string
+  row: number
+  style?: CSSProperties
 }
 
 export default class ArticleListSkeleton extends PureComponent<
-    IArticleListSkeletonProps
+  IArticleListSkeletonProps
 > {
-    public constructor(props: IArticleListSkeletonProps) {
-        super(props)
-    }
-    public render() {
-        const { className, row, style } = this.props
-        const classes = 'article-list-skeleton ' + (className || '')
-        const list = Array(row)
-            .fill(0)
-            .map((el, i) => i)
-        return (
-            <div className={classes} style={style}>
-                <SpanSkeleton width={90} style={{ marginLeft: 8 }} />
-                {list.map(item => (
-                    <ArticleItemSkeleton key={item} />
-                ))}
-            </div>
-        )
-    }
+  public constructor(props: IArticleListSkeletonProps) {
+    super(props)
+  }
+  public render() {
+    const { className, row, style } = this.props
+    const classes = 'article-list-skeleton ' + (className || '')
+    const list = Array(row)
+      .fill(0)
+      .map((el, i) => i)
+    return (
+      <div className={classes} style={style}>
+        <SpanSkeleton width={90} style={{ marginLeft: 8 }} />
+        {list.map(item => (
+          <ArticleItemSkeleton key={item} />
+        ))}
+      </div>
+    )
+  }
 }
--- src/skeletons/AvatarSkeleton.tsx
+++ src/skeletons/AvatarSkeleton.tsx
@@ -2,19 +2,19 @@ import React, { CSSProperties, PureComponent } from 'react'
 import '../styles/Skeletons.less'
 
 interface IAvatarSkeletonProps {
-    className?: string
-    style?: CSSProperties
+  className?: string
+  style?: CSSProperties
 }
 
 export default class AvatarSkeleton extends PureComponent<
-    IAvatarSkeletonProps
+  IAvatarSkeletonProps
 > {
-    public constructor(props: IAvatarSkeletonProps) {
-        super(props)
-    }
-    public render() {
-        const { className, style } = this.props
-        const classes = 'avatar-skeleton skeleton-active ' + (className || '')
-        return <div className={classes} style={style} />
-    }
+  public constructor(props: IAvatarSkeletonProps) {
+    super(props)
+  }
+  public render() {
+    const { className, style } = this.props
+    const classes = 'avatar-skeleton skeleton-active ' + (className || '')
+    return <div className={classes} style={style} />
+  }
 }
