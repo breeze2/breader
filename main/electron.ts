03b5f39a18d1ab2cbdcf4e044b41fdd1a1d6c9f2
Breeze <805511939@qq.com>
2019-08-04 11:22:16 +0800

    chore(main): prettier codes
    refer #24

--- main/electron.ts
+++ main/electron.ts
@@ -8,62 +8,54 @@ import { template as menuTemplate } from './menu'
 let mainWindow: BrowserWindow | null = null
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true,
-            webviewTag: true,
-            // preload: path.join(__dirname, './preload.js'),
-        },
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+      webviewTag: true,
+      // preload: path.join(__dirname, './preload.js'),
+    },
 
-        height: 600,
-        width: 960,
+    height: 600,
+    width: 960,
 
-        titleBarStyle: 'hiddenInset',
-        vibrancy: 'dark',
-    })
+    titleBarStyle: 'hiddenInset',
+    vibrancy: 'dark',
+  })
 
-    // and load the index.html of the app.
-    // mainWindow.loadFile('index.html')
-    const isDev = require('electron-is-dev')
-    if (isDev) {
-        const {
-            default: installExtension,
-            REACT_DEVELOPER_TOOLS,
-            REDUX_DEVTOOLS,
-        } = require('electron-devtools-installer')
-        installExtension(REACT_DEVELOPER_TOOLS)
-            .then((name: string) =>
-                console.info(`Added Extension:  ${name}`)
-            )
-            .catch((err: any) =>
-                console.error('An error occurred: ', err)
-            )
-        installExtension(REDUX_DEVTOOLS)
-            .then((name: string) =>
-                console.info(`Added Extension:  ${name}`)
-            )
-            .catch((err: any) =>
-                console.error('An error occurred: ', err)
-            )
-        // require('devtron').install()
+  // and load the index.html of the app.
+  // mainWindow.loadFile('index.html')
+  const isDev = require('electron-is-dev')
+  if (isDev) {
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+      REDUX_DEVTOOLS,
+    } = require('electron-devtools-installer')
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name: string) => console.info(`Added Extension:  ${name}`))
+      .catch((err: any) => console.error('An error occurred: ', err))
+    installExtension(REDUX_DEVTOOLS)
+      .then((name: string) => console.info(`Added Extension:  ${name}`))
+      .catch((err: any) => console.error('An error occurred: ', err))
+    // require('devtron').install()
 
-        mainWindow.loadURL('http://localhost:3000/?react_perf')
-    } else {
-        mainWindow.loadFile(path.join(__dirname, '/../build/index.html'))
-    }
+    mainWindow.loadURL('http://localhost:3000/?react_perf')
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '/../build/index.html'))
+  }
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
 }
 
 // This method will be called when Electron has finished
@@ -71,25 +63,25 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 app.on('ready', () => {
-    const menu = Menu.buildFromTemplate(menuTemplate)
-    Menu.setApplicationMenu(menu)
+  const menu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(menu)
 })
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
--- main/menu.ts
+++ main/menu.ts
@@ -1,171 +1,168 @@
 import {
-    app,
-    BrowserWindow,
-    Event,
-    MenuItem,
-    MenuItemConstructorOptions,
-    shell,
+  app,
+  BrowserWindow,
+  Event,
+  MenuItem,
+  MenuItemConstructorOptions,
+  shell,
 } from 'electron'
 
 export const template: MenuItemConstructorOptions[] = [
-    {
-        label: 'Edit',
-        submenu: [
-            {
-                accelerator: 'CmdOrCtrl+Z',
-                label: 'Undo',
-                role: 'undo',
-            },
-            {
-                accelerator: 'Shift+CmdOrCtrl+Z',
-                label: 'Redo',
-                role: 'redo',
-            },
-            {
-                type: 'separator',
-            },
-            {
-                accelerator: 'CmdOrCtrl+X',
-                label: 'Cut',
-                role: 'cut',
-            },
-            {
-                accelerator: 'CmdOrCtrl+C',
-                label: 'Copy',
-                role: 'copy',
-            },
-            {
-                accelerator: 'CmdOrCtrl+V',
-                label: 'Paste',
-                role: 'paste',
-            },
-            {
-                accelerator: 'CmdOrCtrl+A',
-                label: 'Select All',
-                role: 'selectall',
-            },
-        ],
-    },
-    {
-        label: 'Help',
-        role: 'help',
-        submenu: [
-            {
-                click: () => {
-                    shell.openExternal('https://github.com/breeze2/breader')
-                },
-                label: 'Learn More',
-            },
-            {
-                accelerator: 'CmdOrCtrl+Alt+I',
-                label: 'Toggle Developer Tools',
-                role: 'toggledevtools',
-            },
-        ],
-    },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        accelerator: 'CmdOrCtrl+Z',
+        label: 'Undo',
+        role: 'undo',
+      },
+      {
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        label: 'Redo',
+        role: 'redo',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        accelerator: 'CmdOrCtrl+X',
+        label: 'Cut',
+        role: 'cut',
+      },
+      {
+        accelerator: 'CmdOrCtrl+C',
+        label: 'Copy',
+        role: 'copy',
+      },
+      {
+        accelerator: 'CmdOrCtrl+V',
+        label: 'Paste',
+        role: 'paste',
+      },
+      {
+        accelerator: 'CmdOrCtrl+A',
+        label: 'Select All',
+        role: 'selectall',
+      },
+    ],
+  },
+  {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        click: () => {
+          shell.openExternal('https://github.com/breeze2/breader')
+        },
+        label: 'Learn More',
+      },
+      {
+        accelerator: 'CmdOrCtrl+Alt+I',
+        label: 'Toggle Developer Tools',
+        role: 'toggledevtools',
+      },
+    ],
+  },
 ]
 
 function addUpdateMenuItems(
-    items: MenuItemConstructorOptions[],
-    position: number
+  items: MenuItemConstructorOptions[],
+  position: number
 ) {
-    if (process.mas) {
-        return
-    }
+  if (process.mas) {
+    return
+  }
 
-    const version = app.getVersion()
-    const updateItems: MenuItemConstructorOptions[] = [
-        {
-            enabled: false,
-            label: `Version ${version}`,
-        },
-    ]
+  const version = app.getVersion()
+  const updateItems: MenuItemConstructorOptions[] = [
+    {
+      enabled: false,
+      label: `Version ${version}`,
+    },
+  ]
 
-    items.splice.apply(items, [position, 0, ...updateItems])
+  items.splice.apply(items, [position, 0, ...updateItems])
 }
 
 function addPreferencesMenu(
-    menus: MenuItemConstructorOptions[],
-    position: number
+  menus: MenuItemConstructorOptions[],
+  position: number
 ) {
-    const settingsMenu = {
-        label: 'Preferences',
-        submenu: [
-            {
-                click: (
-                    menuItem: MenuItem,
-                    browserWindow: BrowserWindow,
-                    event: Event
-                ) => {
-                    if (browserWindow) {
-                        browserWindow.webContents.send(
-                            'PREFERENCES_MODAL',
-                            'OPEN'
-                        )
-                    }
-                },
-                label: 'Settings',
-            },
-        ],
-    }
-    menus.splice.apply(menus, [position, 0, settingsMenu])
+  const settingsMenu = {
+    label: 'Preferences',
+    submenu: [
+      {
+        click: (
+          menuItem: MenuItem,
+          browserWindow: BrowserWindow,
+          event: Event
+        ) => {
+          if (browserWindow) {
+            browserWindow.webContents.send('PREFERENCES_MODAL', 'OPEN')
+          }
+        },
+        label: 'Settings',
+      },
+    ],
+  }
+  menus.splice.apply(menus, [position, 0, settingsMenu])
 }
 
 if (process.platform === 'darwin') {
-    const name = app.getName()
-    template.unshift({
-        label: name,
-        submenu: [
-            {
-                label: `About ${name}`,
-                role: 'about',
-            },
-            {
-                type: 'separator',
-            },
-            {
-                label: 'Services',
-                role: 'services',
-                submenu: [],
-            },
-            {
-                type: 'separator',
-            },
-            {
-                accelerator: 'Command+H',
-                label: `Hide ${name}`,
-                role: 'hide',
-            },
-            {
-                accelerator: 'Command+Alt+H',
-                label: 'Hide Others',
-                role: 'hideothers',
-            },
-            {
-                label: 'Show All',
-                role: 'unhide',
-            },
-            {
-                type: 'separator',
-            },
-            {
-                accelerator: 'Command+Q',
-                click: () => {
-                    app.quit()
-                },
-                label: 'Quit',
-            },
-        ],
-    })
+  const name = app.getName()
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: `About ${name}`,
+        role: 'about',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: 'Services',
+        role: 'services',
+        submenu: [],
+      },
+      {
+        type: 'separator',
+      },
+      {
+        accelerator: 'Command+H',
+        label: `Hide ${name}`,
+        role: 'hide',
+      },
+      {
+        accelerator: 'Command+Alt+H',
+        label: 'Hide Others',
+        role: 'hideothers',
+      },
+      {
+        label: 'Show All',
+        role: 'unhide',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        accelerator: 'Command+Q',
+        click: () => {
+          app.quit()
+        },
+        label: 'Quit',
+      },
+    ],
+  })
 
-    addPreferencesMenu(template[0].submenu as MenuItemConstructorOptions[], 1)
-    addUpdateMenuItems(template[0].submenu as MenuItemConstructorOptions[], 1)
-    // addPreferencesMenu(template, template.length - 1)
+  addPreferencesMenu(template[0].submenu as MenuItemConstructorOptions[], 1)
+  addUpdateMenuItems(template[0].submenu as MenuItemConstructorOptions[], 1)
+  // addPreferencesMenu(template, template.length - 1)
 }
 
 if (process.platform === 'win32') {
-    const helpMenu = template[template.length - 1].submenu
+  const helpMenu = template[template.length - 1].submenu
 
-    addUpdateMenuItems(helpMenu as MenuItemConstructorOptions[], 0)
-    addPreferencesMenu(template, template.length - 1)
+  addUpdateMenuItems(helpMenu as MenuItemConstructorOptions[], 0)
+  addPreferencesMenu(template, template.length - 1)
 }
