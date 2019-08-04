f91a5563b9be0c2fe013347be9817b955407e0ab
Breeze <805511939@qq.com>
2019-08-04 10:52:43 +0800

    chore: prettier codes
    refer #24
    testing

--- src/components/SearchArticleModal.tsx
+++ src/components/SearchArticleModal.tsx
@@ -11,114 +11,125 @@ import '../styles/SearchArticleModal.less'
 const Search = Input.Search
 
 export interface ISearchArticleModalOwnProps {
-    visible: boolean
-    onCancel: (e: any) => any
-    onItemChoose: (index: number) => any
+  visible: boolean
+  onCancel: (e: any) => any
+  onItemChoose: (index: number) => any
 }
 
-export interface ISearchArticleModalReduxDispatch {
-}
+export interface ISearchArticleModalReduxDispatch {}
 
 export interface ISearchArticleModalReduxState {
-    articles: Immutable.List<IArticle>
+  articles: Immutable.List<IArticle>
 }
 
-export interface ISearchArticleModalProps extends ISearchArticleModalOwnProps, ISearchArticleModalReduxDispatch, ISearchArticleModalReduxState {
-}
+export interface ISearchArticleModalProps
+  extends ISearchArticleModalOwnProps,
+    ISearchArticleModalReduxDispatch,
+    ISearchArticleModalReduxState {}
 
 interface ISearchArticleModalState {
-    readonly keywords: string
-    readonly matchedArticles: Immutable.List<IArticle>
+  readonly keywords: string
+  readonly matchedArticles: Immutable.List<IArticle>
 }
 
-class SearchArticleModal extends Component<ISearchArticleModalProps & InjectedIntlProps, ISearchArticleModalState> {
-    public searchArticles: (keywords: string[]) => any
-    public constructor(props: ISearchArticleModalProps & InjectedIntlProps) {
-        super(props)
-        this.state = {
-            keywords: '',
-            matchedArticles: Immutable.List<IArticle>([]),
+class SearchArticleModal extends Component<
+  ISearchArticleModalProps & InjectedIntlProps,
+  ISearchArticleModalState
+> {
+  public searchArticles: (keywords: string[]) => any
+  public constructor(props: ISearchArticleModalProps & InjectedIntlProps) {
+    super(props)
+    this.state = {
+      keywords: '',
+      matchedArticles: Immutable.List<IArticle>([]),
+    }
+    this.searchArticles = Utils.debounce(this._searchArticles, 100)
+    this.searchArticles = this.searchArticles.bind(this)
+  }
+  public handleSubmit = (e: any) => {
+    const keywords = this.state.keywords
+    const matched = this._searchArticles(keywords.split(' '))
+    if (keywords) {
+      this.setState({
+        matchedArticles: matched,
+      })
+    }
+  }
+  public handleChange = (e: any) => {
+    this.setState({
+      keywords: e.target.value,
+    })
+  }
+  public componentDidUpdate() {
+    if (this.props.visible) {
+      setTimeout(() => {
+        const input: any = document.querySelector('.search-article-keywords')
+        if (input) {
+          input.focus()
         }
-        this.searchArticles = Utils.debounce(this._searchArticles, 100)
-        this.searchArticles = this.searchArticles.bind(this)
+      }, 200)
     }
-    public handleSubmit = (e: any) => {
-        const keywords = this.state.keywords
-        const matched = this._searchArticles(keywords.split(' '))
-        if (keywords) {
-            this.setState({
-                matchedArticles: matched,
-            })
+  }
+  public render() {
+    return (
+      <Modal
+        className="search-article-modal"
+        closable={false}
+        width={376}
+        style={{ top: 42 }}
+        visible={this.props.visible}
+        footer={null}
+        onCancel={this.props.onCancel}>
+        <Search
+          className="search-article-keywords"
+          placeholder={this.props.intl.formatMessage({ id: 'keywords' })}
+          value={this.state.keywords}
+          onChange={this.handleChange}
+          onSearch={this.handleSubmit}
+        />
+        <div className="matched-list">
+          {this.state.matchedArticles.map((article: IArticle) => (
+            <div
+              key={article._id}
+              onClick={() => this.props.onItemChoose(article.index as number)}>
+              <ArticleItem
+                author={article.author}
+                guid={article._id}
+                time={article.time}
+                feedId={article.feedId}
+                title={article.title}
+                summary={article.summary}
+                className="item-is-unread"
+              />
+            </div>
+          ))}
+        </div>
+      </Modal>
+    )
+  }
+  private _searchArticles(keys: string[]) {
+    const len = keys.length
+    if (len < 1) {
+      return Immutable.List<IArticle>([])
+    }
+    const matched = this.props.articles.filter(
+      (article: IArticle, index: number) => {
+        const str = article.title + article.author + article.summary
+        let i = 0
+        for (; i < len; i++) {
+          if (str.indexOf(keys[i]) === -1) {
+            break
+          }
         }
-    }
-    public handleChange = (e: any) => {
-        this.setState({
-            keywords: e.target.value,
-        })
-    }
-    public componentDidUpdate() {
-        if (this.props.visible) {
-            setTimeout(() => {
-                const input: any = document.querySelector('.search-article-keywords')
-                if (input) {
-                    input.focus()
-                }
-            }, 200)
+        if (i === len) {
+          article.index = index
+          return true
         }
-    }
-    public render() {
-        return (
-            <Modal className="search-article-modal"
-                closable={false}
-                width={376}
-                style={{top: 42}}
-                visible={this.props.visible}
-                footer={null}
-                onCancel={this.props.onCancel}
-            >
-                <Search className="search-article-keywords"
-                    placeholder={this.props.intl.formatMessage({ id: 'keywords' })}
-                    value={this.state.keywords}
-                    onChange={this.handleChange}
-                    onSearch={this.handleSubmit} />
-                <div className="matched-list">
-                    {this.state.matchedArticles.map((article: IArticle) => (
-                        <div key={article._id} onClick={() => this.props.onItemChoose(article.index as number)}>
-                            <ArticleItem author={article.author}
-                                guid={article._id}
-                                time={article.time}
-                                feedId={article.feedId}
-                                title={article.title}
-                                summary={article.summary}
-                                className='item-is-unread'
-                            />
-                        </div>
-                    ))}
-                </div>
-            </Modal>
-        )
-    }
-    private _searchArticles(keys: string[]) {
-        const len = keys.length
-        if (len < 1) {
-            return Immutable.List<IArticle>([])
-        }
-        const matched = this.props.articles.filter((article: IArticle, index: number) => {
-            const str = article.title + article.author + article.summary
-            let i = 0
-            for (; i < len; i++) {
-                if (str.indexOf(keys[i]) === -1) {
-                    break
-                }
-            }
-            if (i === len) {
-                article.index = index
-                return true
-            }
-            return false
-        })
-        return matched
-    }
+        return false
+      }
+    )
+    return matched
+  }
 }
 
 export default injectIntl(SearchArticleModal)
