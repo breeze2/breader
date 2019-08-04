44ceee02c5ea2b447acbb700f491e5bdc35a172f
Breeze <805511939@qq.com>
2019-08-04 11:25:39 +0800

    chore(saga): prettier codes
    refer #24

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -5,10 +5,12 @@ import RootReducer from './reducers'
 import RootSaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware()
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || (window as any).compose
-const store = composeEnhancers ?
-    createStore(RootReducer, composeEnhancers(applyMiddleware(sagaMiddleware))) :
-    createStore(RootReducer, applyMiddleware(sagaMiddleware))
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+  (window as any).compose
+const store = composeEnhancers
+  ? createStore(RootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)))
+  : createStore(RootReducer, applyMiddleware(sagaMiddleware))
 
 sagaMiddleware.run(RootSaga)
 
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,19 +1,30 @@
 import { all, fork } from 'redux-saga/effects'
-import { watchFetchArticles, watchFilterArticles, watchSelectAndReadArticles, watchSetAllArticlesRead, watchStarArticle } from './articles'
-import { watchDeleteFeeds, watchFetchFeeds, watchParseFeed, watchUpdateFeeds } from './feeds'
+import {
+  watchFetchArticles,
+  watchFilterArticles,
+  watchSelectAndReadArticles,
+  watchSetAllArticlesRead,
+  watchStarArticle,
+} from './articles'
+import {
+  watchDeleteFeeds,
+  watchFetchFeeds,
+  watchParseFeed,
+  watchUpdateFeeds,
+} from './feeds'
 import { watchSelectMenuKey } from './menu'
 
-export default function* () {
-    yield all([
-        fork(watchSetAllArticlesRead),
-        fork(watchStarArticle),
-        fork(watchSelectAndReadArticles),
-        fork(watchFilterArticles),
-        fork(watchFetchArticles),
-        fork(watchDeleteFeeds),
-        fork(watchFetchFeeds),
-        fork(watchParseFeed),
-        fork(watchUpdateFeeds),
-        fork(watchSelectMenuKey),
-    ])
+export default function*() {
+  yield all([
+    fork(watchSetAllArticlesRead),
+    fork(watchStarArticle),
+    fork(watchSelectAndReadArticles),
+    fork(watchFilterArticles),
+    fork(watchFetchArticles),
+    fork(watchDeleteFeeds),
+    fork(watchFetchFeeds),
+    fork(watchParseFeed),
+    fork(watchUpdateFeeds),
+    fork(watchSelectMenuKey),
+  ])
 }
