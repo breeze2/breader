ea403c2eb67481ffc36675d85914edbe75080621
Breeze <805511939@qq.com>
2019-08-15 00:59:20 +0800

    test(travis): travis ci flow

--- scripts/electron-pack.sh
+++
@@ -1,1 +0,0 @@
-yarn electron-pack
--- scripts/sentry-release.sh
+++
@@ -1,5 +0,0 @@
-# sentry-cli releases new $SENTRY_RELEASE
-# sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps --rewrite ./build
-# sentry-cli releases finalize $SENTRY_RELEASE
-echo 1
-echo $SENTRY_RELEASE
