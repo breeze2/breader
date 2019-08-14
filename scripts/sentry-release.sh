45f1d22568a7ba0967610f40b45012112061e9c3
Breeze <805511939@qq.com>
2019-08-15 00:28:46 +0800

    test(travis): travis ci flow

---
+++ scripts/sentry-release.sh
@@ -0,0 +1,5 @@
+# sentry-cli releases new $SENTRY_RELEASE
+# sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps --rewrite ./build
+# sentry-cli releases finalize $SENTRY_RELEASE
+echo 1
+echo $SENTRY_RELEASE
