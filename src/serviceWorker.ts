598a1e9396fc5795b238247dd0f483d8cdb6961d
breeze2 <805511939@qq.com>
2019-08-15 15:31:16 +0800

    test(travis): travis ci flow

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -20,7 +20,7 @@ const isLocalhost = Boolean(
     )
 )
 
-interface Config {
+type Config = {
   onSuccess?: (registration: ServiceWorkerRegistration) => void
   onUpdate?: (registration: ServiceWorkerRegistration) => void
 }
@@ -49,7 +49,7 @@ export function register(config?: Config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.info(
+          console.log(
             'This web app is being served cache-first by a service ' +
               'worker. To learn more, visit https://bit.ly/CRA-PWA'
           )
@@ -77,7 +77,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.info(
+              console.log(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               )
@@ -90,7 +90,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.info('Content is cached for offline use.')
+              console.log('Content is cached for offline use.')
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -128,7 +128,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
       }
     })
     .catch(() => {
-      console.error(
+      console.log(
         'No internet connection found. App is running in offline mode.'
       )
     })
